7f8c408eb02be6f333c9fdd8fe63b6426c68073c
bensigo <sigocode@gmail.com>
2023-04-03 16:03:23 +0400

    feat: add support for env variable

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 
 function test(){
+    const port = process.env.PORT
+    const appName = process.env.APP_NAME
     console.log("Hello world yfdyf");  
+    console.log(`${appName} on  ${port}`)
 }
 
 test()
